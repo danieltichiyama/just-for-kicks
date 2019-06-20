22ed2e580b145334522ed345c816b16cc3322476
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-06-19 22:17:56 -1000

    still working...

--- js/app.js
+++ js/app.js
@@ -64,7 +64,7 @@ for (i=0;i<plusIcons.length;i++){
     // console.log(plusIcons[i]);
     plusIcons[i].addEventListener('click', goUp);
     plusIcons[i].count = parseInt(plusIcons[i].parentElement.childNodes[0].textContent);
-    console.log (plusIcons[i].count);
+    // console.log (plusIcons[i].count);
 }
 
 
@@ -81,18 +81,16 @@ function goUp (cost){
 
 //6. Add an event to the minus icon that will decrement the price of the Air Jordan VI shoe each time the icon is clicked on.
 
-var plusIcons = document.querySelectorAll ('#decrease');
+var minusIcons = document.querySelectorAll ('#decrease');
 
 for (i=0;i<plusIcons.length;i++){
     // console.log(plusIcons[i]);
-    plusIcons[i].addEventListener('click', goUp);
-    plusIcons[i].count = parseInt(plusIcons[i].parentElement.childNodes[0].textContent);
-    console.log (plusIcons[i].count);
+    minusIcons[i].addEventListener('click', goDown);
+    minusIcons[i].count = parseInt(minusIcons[i].parentElement.childNodes[0].textContent);
+    // console.log (minusIcons[i].count);
 }
 
-
-
-function goUp (cost){
+function goDown (cost){
     console.log('count before ++', this.count)
     console.log (this.parentElement.childNodes);
     console.log(this.count);
@@ -104,8 +102,61 @@ function goUp (cost){
 
 //7. Add an event to the Air Jordan XI shoe that will show another colorway for that shoe after hovering over the image. 
 
+var imageElem= document.querySelectorAll ('img');
+
+var airJordan9 = imageElem[6].parentElement;
+
+var colorData = document.createAttribute ('data-colors');
+colorData.value = "red,white";
+airJordan9.setAttributeNode (colorData);
+
+
+// console.log(imageElem[6].parentElement);
+
+var colorSs = imageElem[6].parentElement.dataset.colors;
+
+var colorArr = colorSs.split(",");
+
+// console.log(colorArr);
+
+debugger;
+for (i=0;i<imageElem.length;i++){
+    console.log(imageElem[i]);
+    // if (imageElem[i].parentElement.dataset.colors==undefined){
+    //     console.log('no other colors to choose from.',imageElem[i]);
+    // }else{
+}
+// imageElem[i].addEventListener ('click',colorChoice);
+// imageElem[i].parentElement.style.position = 'relative';
+
+// var colorSelectBox = document.createElement ('div');
+// colorSelectBox.className = 'colorSelector';
+// colorSelectBox.style.cssText = 'height:10%;width:100%;position:absolute;top:0;right:0;background-color:white;border:1px black solid;display:inherit;z-index:100;dislay:flex;flex-direction:row;';
+
+// imageElem[i].parentElement.appendChild (colorSelectBox);
+
+// for (i=0;i<colorArr.length;i++){
+//     var colorBox = document.createElement ('div');
+//     colorBox.className = 'colorBox';
+//     colorBox.style.cssText = 'height:5px;width:5px;'
+//     colorBox.style.backgroundColor = colorArr[i];
+//     colorSelectBox.appendChild (colorBox);
+//     }
+//     }
+
+// }
+
+
+function colorChoice (){
+    console.log('colorChoice');
+}
+
+
+
 //8. Add an event to the Air Jordan XII shoe that will increase the image by 50% after clicking on the image.
 
 //9. Add events to both the plus and minus icon that will add or decrease the price each time the respective icon is clicked on.
+
+
 
 //10. Add an event to the Air Jordan XXVIII shoe that will show the famous 'crying Michael Jordan' meme after clicking on the image.
